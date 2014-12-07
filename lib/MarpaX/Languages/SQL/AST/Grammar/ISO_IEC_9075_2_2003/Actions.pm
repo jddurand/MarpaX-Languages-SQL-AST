02448c9fbfb4e661784ac06f03024bb4f8d5bed6
Jean-Damien Durand <jeandamiendurand@free.fr>
2014-12-07 09:45:54 +0100

    ISO_IEC_9075_2_2003/Actions.pm: use parent

--- lib/MarpaX/Languages/SQL/AST/Grammar/ISO_IEC_9075_2_2003/Actions.pm
+++ lib/MarpaX/Languages/SQL/AST/Grammar/ISO_IEC_9075_2_2003/Actions.pm
@@ -2,8 +2,10 @@ use strict;
 use warnings FATAL => 'all';
 
 package MarpaX::Languages::SQL::AST::Grammar::ISO_IEC_9075_2_2003::Actions;
+use parent 'MarpaX::Languages::SQL::AST::Grammar::Actions';
+use SUPER;
 
-# ABSTRACT: SQL 2003 grammar actions
+# ABSTRACT: SQL 2003 grammar generic actions
 
 # VERSION
 
@@ -13,11 +15,11 @@ This modules give the actions associated to ISO_IEC_9075_2_2003 grammar.
 
 =cut
 
-sub new {
-    my $class = shift;
-    my $self = {};
-    bless($self, $class);
-    return $self;
-}
+#
+# Done like this because Marpa is using $CODE{}
+
+sub new                 { super() }
+sub nonTerminalSemantic { super() }
+sub fakedLexeme         { super() }
 
 1;
